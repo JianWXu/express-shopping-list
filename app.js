e95898558b8e1bd95b950a7f07a89b05182aee1c
JianWXu <jianwxu88@gmail.com>
2023-09-28 22:20:59 -0700

    all routes done
    nothing tested

--- app.js
+++ app.js
@@ -9,22 +9,8 @@ const e = require("express");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// directory for get /item routers
+// directory for /item routers
 app.use("/items", router);
-
-// directory for post /items
-app.post("/items", function (req, res, next) {
-  try {
-    if (!req.body) {
-      throw new ExpressError("Please enter a shopping list item", 400);
-      const newItem = { name: req.body.name, price: req.body.price };
-      items.push(newItem);
-      res.status(201).json(newItem);
-    }
-  } catch (e) {
-    next(e);
-  }
-});
 
 app.use(function (req, res, next) {
   const notFoundError = new ExpressError("Not Found", 404);
--- routers.js
+++ routers.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 
 const items = require("./fakeDB");
+const ExpressError = require("./expressErrors");
 
 // route for getting json of all shopping list items:
 
@@ -14,6 +15,38 @@ router.get("/", function (req, res) {
 router.get("/:name", function (req, res) {
   let item = items.find((i) => i.name === req.params.name);
   return res.json(item);
+});
+
+router.post("/", function (req, res) {
+  try {
+    if (!req.body) {
+      throw new ExpressError("Please enter a shopping list item", 400);
+      const newItem = { name: req.body.name, price: req.body.price };
+      items.push(newItem);
+      res.status(201).json(newItem);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.patch("/:name", function (req, res) {
+  if (!req.body) {
+    throw new ExpressError("Please enter a shopping list item", 400);
+    const item = items.find((i) => (i.name = req.params.name));
+    item.name = req.body.name;
+    item.price = req.body.price;
+    res.status(200).json(item);
+  }
+});
+
+router.delete("/:name", function (req, res) {
+  if (!items[req.params.name]) {
+    throw new ExpressError("Please enter a valid shopping list item", 404);
+    const item = items.findIndex((i) => (i.name = req.params.name));
+    items.splice(item, 1);
+    res.json({ message: "deleted" });
+  }
 });
 
 module.exports = router;
